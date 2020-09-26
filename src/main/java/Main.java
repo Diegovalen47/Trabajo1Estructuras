5a5ae6c97d4d21860104ecdec990dfca7aa55f8f
Valentín Osorio <dosoriom@unal.edu.co>
2020-09-25 21:50:54 -0500

    cambii¿o

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonPrimitive;
+import java.util.Map.Entry;
 
 public class Main {
     Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        int prueba = 1
+        int prueba = 1;
+        JsonParser parser = new JsonParser();
+
     }
 }
