866c3dad6eab101f310101b727ec4519f93d7684
Valentín Osorio <dosoriom@unal.edu.co>
2020-11-23 01:39:09 -0500

    Añadí ventana de iniciar sesión

--- src/main/java/App/controladores/MenuPrincipalController.java
+++ src/main/java/App/controladores/MenuPrincipalController.java
@@ -1,17 +1,86 @@
 package App.controladores;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MenuPrincipalController {
 
+    public static String eMail = "dosoriom@unal.edu.co";
+    public static String user = "dosoriom";
+    public static String password = "estructuras";
+
+    @FXML
     public Button closeButton;
+
+    @FXML
+    public TextField TextUsuarioCorreo;
+
+    @FXML
+    public PasswordField Clave;
+
+    @FXML
+    public Label WarningMessage;
+
     @FXML
     private void closeButtonAction(){
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
     }
 
+    @FXML
+    public void IniciarSesion(ActionEvent event) throws IOException {
+        WarningMessage.setVisible(false);
+        String usuario = TextUsuarioCorreo.getText().trim();
+        String clave = Clave.getText();
+
+
+        if (usuario.equals("") || clave.equals("")) {
+            WarningMessage.setText("Los campos no pueden estar vacios");
+            WarningMessage.setVisible(true);
+            return;
+        }
+
+
+
+        if (usuario.contains("@")) {
+            if (usuario.equals(eMail)) {
+                if (clave.equals(password)) {
+
+                } else {
+                    WarningMessage.setText("Contraseña incorrecta");
+                    WarningMessage.setVisible(true);
+                    return;
+                }
+            } else {
+                WarningMessage.setText("El usuario no existe");
+                WarningMessage.setVisible(true);
+                return;
+            }
+        } else if (usuario.equals(user)) {
+            if (clave.equals(password)) {
+
+            } else {
+                WarningMessage.setText("Contraseña incorrecta");
+                WarningMessage.setVisible(true);
+                return;
+            }
+        } else {
+            WarningMessage.setText("El usuario no existe");
+            WarningMessage.setVisible(true);
+            return;
+        }
+
+        WarningMessage.setVisible(false);
+        TextUsuarioCorreo.setText("");
+        Clave.setText("");
+
+        //se valida y luego se va a la siguiente ventana
+        
+
+    }
 
 }
